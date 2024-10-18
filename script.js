36d8e560c20b83fd772530f716e0a49d3f67abb3
Mehendi Sil <115357567+mehendisil2004@users.noreply.github.com>
2024-10-19 00:38:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const menu = document.getElementById("menu");
+const navLinks = document.querySelector(".nav-links");
+
+menu.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+menuIcon.onclick= () => {
+  navLinks.classList.toggle("active");
+}
